0e07aabe969d3cbd9d4fd2263aab7382629d4d38
Eric-Guo <eric.guocz@gmail.com>
2025-12-25 17:46:50 +0800

    Display Chinese name and get keys from SSO
    Rename installation artifactName
    Enable dev branch auto update

--- packages/sdk/js/src/v2/gen/types.gen.ts
+++ packages/sdk/js/src/v2/gen/types.gen.ts
@@ -1816,6 +1816,7 @@ export type Config = {
   small_model?: string
   default_agent?: string
   username?: string
+  clerk_code?: string
   mode?: {
     build?: AgentConfig
     plan?: AgentConfig
