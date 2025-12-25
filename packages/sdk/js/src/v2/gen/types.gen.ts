354b04d6264b8f29ef62264af636020608577421
Eric-Guo <eric.guocz@gmail.com>
2025-12-25 17:46:50 +0800

    Display Chinese name and get keys from SSO

--- packages/sdk/js/src/v2/gen/types.gen.ts
+++ packages/sdk/js/src/v2/gen/types.gen.ts
@@ -2234,6 +2234,10 @@ export type Config = {
    */
   username?: string
   /**
+   * THAPE clerk code. Auto-populated from SSO when available
+   */
+  clerk_code?: string
+  /**
    * @deprecated Use `agent` field instead.
    */
   mode?: {
