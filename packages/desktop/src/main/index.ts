82c74a6bb05c92cb16f4725dd1bd110d142a09ce
Eric-Guo <eric.guocz@gmail.com>
2025-12-25 17:46:50 +0800

    Display Chinese name and get keys from SSO
    Rename installation artifactName
    Enable dev branch auto update
    pack as SigmaAgents

--- packages/desktop/src/main/index.ts
+++ packages/desktop/src/main/index.ts
@@ -1,4 +1,5 @@
 import { app, session } from "electron"
+import { ensureSsoUsername } from "../../../opencode/src/util/thape_sso"
 import { Deferred, Effect, Fiber } from "effect"
 import type { ServerReadyData } from "../shared/ipc-contract"
 import { checkAppExists, resolveAppPath } from "./files/apps"
@@ -97,6 +98,7 @@ const main = Effect.gen(function* () {
       hasBypassRules: Boolean(sessionProxy.proxyBypassRules),
     })
   startAutoUpdater(updater)
+  yield* Effect.promise(() => ensureSsoUsername())
   yield* Effect.promise(() => startNetworkLogging())
 
   const loadingTask = yield* Effect.gen(function* () {
