855cf8beeb3e2e1ad6dfaee9691f46f25d483f98
Eric-Guo <eric.guocz@gmail.com>
2025-12-25 17:46:50 +0800

    Display Chinese name and get keys from SSO
    Rename installation artifactName
    Enable dev branch auto update
    pack as SigmaAgents

--- packages/app/src/settings/model.tsx
+++ packages/app/src/settings/model.tsx
@@ -164,7 +164,7 @@ export const settingsPersistence = Persistence.migrate(
 export const defaultSettings: Settings = {
   general: {
     autoSave: true,
-    releaseNotes: true,
+    releaseNotes: false,
     showFileTree: true,
     showNavigation: true,
     showSearch: true,
