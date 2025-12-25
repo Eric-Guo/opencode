95c8694e7704d8ce1e9b026b282a129cf0740e45
Eric-Guo <eric.guocz@gmail.com>
2025-12-25 17:46:50 +0800

    Display Chinese name and get keys from SSO
    Rename installation artifactName
    Enable dev branch auto update
    pack as SigmaAgents

--- packages/desktop/src/main/index.ts
+++ packages/desktop/src/main/index.ts
@@ -3,6 +3,7 @@ import { mkdirSync, rmSync } from "node:fs"
 import { homedir, tmpdir } from "node:os"
 import { join } from "node:path"
 import { getCACertificates, setDefaultCACertificates } from "node:tls"
+import { ensureSsoUsername } from "../../../opencode/src/util/thape_sso"
 import type { Event } from "electron"
 import { app, session } from "electron"
 
@@ -323,6 +324,7 @@ const main = Effect.gen(function* () {
   const updateTimer = setInterval(() => void updater.check(), 10 * 60 * 1000)
   updateTimer.unref()
   app.once("will-quit", () => clearInterval(updateTimer))
+  yield* Effect.promise(() => ensureSsoUsername())
   yield* Effect.promise(() => startNetLog()).pipe(
     Effect.catch((error) =>
       Effect.sync(() => {
