1f00befb8487e70b5b1a6355124c8715222760da
Eric-Guo <eric.guocz@gmail.com>
2025-12-25 17:46:50 +0800

    Display chinese name and get keys from SSO

--- packages/opencode/src/index.ts
+++ packages/opencode/src/index.ts
@@ -5,6 +5,7 @@ import { GenerateCommand } from "./cli/cmd/generate"
 import { Log } from "./util/log"
 import { ConsoleCommand } from "./cli/cmd/account"
 import { ProvidersCommand } from "./cli/cmd/providers"
+import { ensureSsoUsername } from "./util/thape_sso"
 import { AgentCommand } from "./cli/cmd/agent"
 import { UpgradeCommand } from "./cli/cmd/upgrade"
 import { UninstallCommand } from "./cli/cmd/uninstall"
@@ -97,6 +98,7 @@ const cli_init = yargs(args)
         return "INFO"
       })(),
     })
+    await ensureSsoUsername()
 
     Heap.start()
 
