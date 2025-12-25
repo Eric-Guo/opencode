c04fb4fa6bf44a1244981a711d7a48ec457b56f5
Eric-Guo <eric.guocz@gmail.com>
2025-12-25 17:46:50 +0800

    Display chinese name and get keys from SSO

--- packages/app/src/components/settings-general.tsx
+++ packages/app/src/components/settings-general.tsx
@@ -458,8 +458,6 @@ export const SettingsGeneral: Component = () => {
           }}
         </Show>
 
-        <UpdatesSection />
-
         <Show when={linux()}>
           {(_) => {
             const [valueResource, actions] = createResource(() => platform.getDisplayBackend?.())
