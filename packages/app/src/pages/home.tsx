9a2f01c44da7f1d031b304b662a77c550ef79a6d
Eric-Guo <eric.guocz@gmail.com>
2026-01-02 22:02:14 +0800

    Title as Chinese.

--- packages/app/src/pages/home.tsx
+++ packages/app/src/pages/home.tsx
@@ -202,7 +202,7 @@ function HomeDesign() {
         clearNotifications={notification.project.markViewed}
         unseenCount={notification.project.unseenCount}
         openSettings={openSettings}
-        openHelp={() => platform.openLink("https://opencode.ai/desktop-feedback")}
+        openHelp={() => platform.openLink("https://plm.thape.com.cn/projects/opencode/wiki/01-shi-yong-shuo-ming")}
         language={language}
       />
 
