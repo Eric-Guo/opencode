431f363b23c6b94b78c3c5e8584eaf2350602001
Eric-Guo <eric.guocz@gmail.com>
2026-01-02 22:02:14 +0800

    Title as Chinese.

--- packages/app/src/pages/layout.tsx
+++ packages/app/src/pages/layout.tsx
@@ -2031,7 +2031,7 @@ export default function Layout(props: ParentProps) {
               settingsKeybind={() => command.keybind("settings.open")}
               onOpenSettings={openSettings}
               helpLabel={() => language.t("sidebar.help")}
-              onOpenHelp={() => platform.openLink("https://opencode.ai/desktop-feedback")}
+              onOpenHelp={() => platform.openLink("https://plm.thape.com.cn/projects/opencode/wiki/01-shi-yong-shuo-ming")}
               renderPanel={() => <SidebarPanel project={currentProject()} />}
             />
           </div>
@@ -2096,7 +2096,7 @@ export default function Layout(props: ParentProps) {
               settingsKeybind={() => command.keybind("settings.open")}
               onOpenSettings={openSettings}
               helpLabel={() => language.t("sidebar.help")}
-              onOpenHelp={() => platform.openLink("https://opencode.ai/desktop-feedback")}
+              onOpenHelp={() => platform.openLink("https://plm.thape.com.cn/projects/opencode/wiki/01-shi-yong-shuo-ming")}
               renderPanel={() => <SidebarPanel project={currentProject()} mobile />}
             />
           </nav>
