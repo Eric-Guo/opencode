4ceec2056cf2732c592df1584c46ab98666b367f
Eric-Guo <eric.guocz@gmail.com>
2026-01-12 21:17:11 +0800

    Display chinese name and clerk code in Desktop

--- packages/ui/src/context/data.tsx
+++ packages/ui/src/context/data.tsx
@@ -1,9 +1,10 @@
-import type { Message, Session, Part, FileDiff, SessionStatus, ProviderListResponse } from "@opencode-ai/sdk/v2"
+import type { Message, Session, Part, FileDiff, SessionStatus, ProviderListResponse, Config } from "@opencode-ai/sdk/v2"
 import { createSimpleContext } from "./helper"
 import { PreloadMultiFileDiffResult } from "@pierre/diffs/ssr"
 
 type Data = {
   provider?: ProviderListResponse
+  config?: Config
   session: Session[]
   session_status: {
     [sessionID: string]: SessionStatus
