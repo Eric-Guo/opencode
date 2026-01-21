98aef761b77cb466b0301f00a5a43cd832951894
Eric-Guo <eric.guocz@gmail.com>
2026-01-21 11:23:43 +0800

    Remove upgrade and uninstall for local/preview build.

--- packages/opencode/src/index.ts
+++ packages/opencode/src/index.ts
@@ -47,7 +47,7 @@ process.on("uncaughtException", (e) => {
   })
 })
 
-let cli = yargs(hideBin(process.argv))
+const cli_init = yargs(hideBin(process.argv))
   .parserConfiguration({ "populate--": true })
   .scriptName("opencode")
   .wrap(100)
@@ -136,8 +136,6 @@ let cli = yargs(hideBin(process.argv))
   .command(WorkspacesCommand)
   .command(ProvidersCommand)
   .command(AgentCommand)
-  .command(UpgradeCommand)
-  .command(UninstallCommand)
   .command(ServeCommand)
   .command(WebCommand)
   .command(ModelsCommand)
@@ -149,11 +147,12 @@ let cli = yargs(hideBin(process.argv))
   .command(SessionCommand)
   .command(DbCommand)
 
-if (Installation.isLocal()) {
-  cli = cli.command(WorkspaceServeCommand)
-}
+const cli =
+  Installation.isLocal() || Installation.isPreview()
+    ? cli_init.command(WorkspaceServeCommand)
+    : cli_init.command(UpgradeCommand).command(UninstallCommand)
 
-cli = cli
+cli
   .fail((msg, err) => {
     if (
       msg?.startsWith("Unknown argument") ||
