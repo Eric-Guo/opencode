a6b7bfb0b369be24df3911347d996ec456fd5d9b
Eric-Guo <eric.guocz@gmail.com>
2026-02-06 23:00:29 +0800

    Add session_cwd agent option

--- packages/sdk/js/src/v2/gen/types.gen.ts
+++ packages/sdk/js/src/v2/gen/types.gen.ts
@@ -1468,6 +1468,10 @@ export type AgentConfig = {
   variant?: string
   temperature?: number
   top_p?: number
+  /**
+   * Override the working directory for new ACP sessions
+   */
+  session_cwd?: string
   prompt?: string
   /**
    * @deprecated Use 'permission' field instead
@@ -2267,6 +2271,7 @@ export type Agent = {
   hidden?: boolean
   topP?: number
   temperature?: number
+  session_cwd?: string
   color?: string
   permission: PermissionRuleset
   model?: {
