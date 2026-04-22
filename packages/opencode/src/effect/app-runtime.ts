e877ec89b6fdbe7ea366d8a84dcca84bd0e472e9
Eric-Guo <eric.guocz@gmail.com>
2026-04-22 21:31:17 +0800

    Allow global config without instance context

--- packages/opencode/src/effect/app-runtime.ts
+++ packages/opencode/src/effect/app-runtime.ts
@@ -66,7 +66,7 @@ const ConfigWithPluginPriority = Layer.effect(
     return {
       ...config,
       get: () => Effect.andThen(plugin.init(), config.get),
-      getGlobal: () => Effect.andThen(plugin.init(), config.getGlobal),
+      getGlobal: config.getGlobal,
       getConsoleState: () => Effect.andThen(plugin.init(), config.getConsoleState),
     }
   }),
