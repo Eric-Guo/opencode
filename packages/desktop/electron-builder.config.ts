c4e570fec11a26cd548b150bc8f083f51e70f4be
Eric-Guo <eric.guocz@gmail.com>
2026-05-07 15:02:52 +0800

    Install thape-config dependencies before packaging

--- packages/desktop/electron-builder.config.ts
+++ packages/desktop/electron-builder.config.ts
@@ -110,6 +110,11 @@ const getBase = (appId: string): Configuration => ({
       to: "thape-config",
       filter: ["**/*", "!**/.git/**"],
     },
+    {
+      from: "resources/thape-config/node_modules",
+      to: "thape-config/node_modules",
+      filter: ["**/*"],
+    },
   ],
   afterPack: async (context) => {
     const cli = path.join(
