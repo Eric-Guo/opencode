98f49ceafc3e4e0419354e9a3afaa236824f07d0
Eric-Guo <eric.guocz@gmail.com>
2026-06-11 07:34:53 +0800

    Fix storybook turbo build errors.

--- packages/storybook/.storybook/mocks/solid-router.tsx
+++ packages/storybook/.storybook/mocks/solid-router.tsx
@@ -1,4 +1,4 @@
-import type { ParentProps } from "solid-js"
+import type { JSX, ParentProps } from "solid-js"
 
 export function useParams() {
   return {
@@ -9,6 +9,10 @@ export function useParams() {
 
 export function useNavigate() {
   return () => undefined
+}
+
+export function useSearchParams<T extends Record<string, string | undefined>>() {
+  return [{} as T, () => undefined] as const
 }
 
 export function useLocation() {
@@ -23,6 +27,28 @@ export function MemoryRouter(props: ParentProps) {
   return props.children
 }
 
+export function Router(props: ParentProps) {
+  return props.children
+}
+
 export function Route(props: ParentProps) {
   return props.children
 }
+
+export function Navigate() {
+  return undefined
+}
+
+export function A(props: ParentProps<{ href?: string; class?: string; classList?: Record<string, boolean> }>) {
+  return <a href={props.href} class={props.class} classList={props.classList}>{props.children}</a>
+}
+
+export function useMatch() {
+  return () => undefined
+}
+
+export function useIsRouting() {
+  return false
+}
+
+export type BaseRouterProps = ParentProps<{ children?: JSX.Element }>
