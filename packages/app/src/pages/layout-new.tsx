22b1ba4ddb8dcfc3a764996d302e8528ef68d47b
Eric-Guo <eric.guocz@gmail.com>
2026-06-23 17:45:52 +0800

    Hide help button.

--- packages/app/src/pages/layout-new.tsx
+++ packages/app/src/pages/layout-new.tsx
@@ -31,7 +31,6 @@ export default function NewLayout(props: ParentProps) {
         {props.children}
       </main>
       {import.meta.env.DEV && <DebugBar />}
-      <HelpButton />
       <ToastRegion v2 />
     </div>
   )
