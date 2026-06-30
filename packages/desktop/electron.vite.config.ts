9b1b09940ce64f25aa07e69efe630f6ae6b47688
Eric-Guo <eric.guocz@gmail.com>
2026-06-30 09:21:02 +0800

    feat(desktop): Introduce tab to loading different website.
    Preserve released external tab urls
    Persist external tab cookies across restarts
    Only persist valid external tab urls
    Always open external view links in browser
    Load tab definitions dynamically

--- packages/desktop/electron.vite.config.ts
+++ packages/desktop/electron.vite.config.ts
@@ -83,7 +83,7 @@ const require = __cjs_mod__.createRequire(import.meta.url);
   preload: {
     build: {
       rollupOptions: {
-        input: { index: "src/preload/index.ts" },
+        input: { index: "src/preload/index.ts", tabbar: "src/preload/tabbar.ts" },
         output: {
           format: "cjs",
           entryFileNames: "[name].js",
@@ -111,6 +111,7 @@ const require = __cjs_mod__.createRequire(import.meta.url);
       rollupOptions: {
         input: {
           main: "src/renderer/index.html",
+          tabbar: "src/renderer/tabbar.html",
         },
       },
     },
