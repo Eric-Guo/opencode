b7446632bd5266c06f177a76dc9f3374913012e2
Eric-Guo <eric.guocz@gmail.com>
2026-07-09 10:32:38 +0800

    Revert "fix(core): watch only git projects (#35957)"
    This reverts commit f95f877e5f7c8cc6eab4f920e2925572d69b015b.

--- packages/core/test/filesystem/watcher.test.ts
+++ packages/core/test/filesystem/watcher.test.ts
@@ -300,12 +300,16 @@ describeWatcher("LocationWatcher", () => {
     ),
   )
 
-  it.live("skips non-git roots", () =>
+  it.live("watches non-git roots", () =>
     withTmp((directory) =>
       Effect.gen(function* () {
         const fs = yield* FSUtil.Service
         const file = path.join(directory, "plain.txt")
-        yield* noUpdate((event) => event.file === file, fs.writeFileString(file, "plain"))
+        yield* ready(directory)
+        expect(yield* nextUpdate((event) => event.file === file, fs.writeFileString(file, "plain"))).toEqual({
+          file,
+          event: "add",
+        })
       }),
     ),
   )
@@ -338,10 +342,7 @@ describeWatcher("LocationWatcher", () => {
         Effect.promise(() => tmpdir()),
         (tmp) => Effect.promise(() => tmp[Symbol.asyncDispose]()),
       )
-      yield* ready(tmp.path).pipe(
-        provide(tmp.path, { type: "git", store: AbsolutePath.make(path.join(tmp.path, ".git")) }),
-        Effect.scoped,
-      )
+      yield* ready(tmp.path).pipe(provide(tmp.path), Effect.scoped)
       const file = path.join(tmp.path, "after-dispose.txt")
       yield* noUpdate((event) => event.file === file, fs.writeFileString(file, "gone")).pipe(
         Effect.provideService(Bus.Service, bus),
