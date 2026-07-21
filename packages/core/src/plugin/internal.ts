3e79ccce1dbdeb07058f62bbdf6169f0ba5f34c9
Eric-Guo <eric.guocz@gmail.com>
2026-07-21 13:13:23 +0800

    feat: migrate eric changes to v2

--- packages/core/src/plugin/internal.ts
+++ packages/core/src/plugin/internal.ts
@@ -94,6 +94,8 @@ import { VariantPlugin } from "./variant.js"
 import { VcsGitPlugin } from "./vcs/git.js"
 import { WarmingPlugin } from "./warming.js"
 import { WellKnownPlugin } from "../wellknown/plugin.js"
+import { CybrosTrace } from "./cybros.js"
+import { SessionStore } from "../session/store.js"
 
 const services = [
   Agent.Service,
@@ -130,6 +132,7 @@ const services = [
   Session.Service,
   SessionCompaction.Service,
   SessionInstructions.Service,
+  SessionStore.Service,
   Shell.Service,
   ShellSelect.Service,
   Snapshot.Service,
@@ -179,6 +182,7 @@ export const requirements = LayerNode.group([
   Session.node,
   SessionCompaction.node,
   SessionInstructions.node,
+  SessionStore.node,
   Shell.node,
   ShellSelect.node,
   Snapshot.node,
@@ -205,6 +209,7 @@ const pre = [
   SkillPlugin.Plugin,
   VcsHgPlugin.Plugin,
   ModelsDevPlugin,
+  CybrosTrace.Plugin,
   ...ProviderPlugins,
   ...WebSearchPlugins,
   PatchTool.Plugin,
