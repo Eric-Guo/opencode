043c85af3193917e2a26d71708830cb2fe077e07
Eric-Guo <eric.guocz@gmail.com>
2026-07-21 13:13:23 +0800

    feat: migrate eric changes to v2

--- packages/server/src/process.ts
+++ packages/server/src/process.ts
@@ -46,7 +46,7 @@ const errorResponseLogger = HttpMiddleware.make((app) =>
   ),
 )
 
-export const start = Effect.fn("ServerProcess.start")(function* <E, R>(
+export const start = Effect.fn("ServerProcess.start")(function* <E = never, R = never>(
   options: ServerOptions,
   lifecycle?: Lifecycle<E, R>,
   transform?: Transform,
