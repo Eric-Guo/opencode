8bd1b14a164378985128f14f11a7bb14b38eeaab
Eric-Guo <eric.guocz@gmail.com>
2026-07-21 13:13:23 +0800

    feat: migrate eric changes to v2

--- packages/cli/src/server-process.ts
+++ packages/cli/src/server-process.ts
@@ -15,6 +15,7 @@ import { ServiceConfig } from "./services/service-config"
 import { ServiceRegistration } from "./services/service-registration"
 import { WebUi } from "./services/web-ui"
 import { databasePath } from "./database-path"
+import { configDirectory } from "./config-directory"
 
 export type Mode = "default" | "service" | "stdio"
 
@@ -30,11 +31,7 @@ export const run = Effect.fnUntraced(function* (options: Options) {
   return yield* processEffect(options).pipe(
     Effect.provide(
       LayerNode.compile(LayerNode.group([Global.node, AppProcess.node]), {
-        replacements: [
-          Global.node.replace(
-            Global.layerWith(process.env.OPENCODE_CONFIG_DIR ? { config: process.env.OPENCODE_CONFIG_DIR } : {}),
-          ),
-        ],
+        replacements: [Global.node.replace(Global.layerWith({ config: configDirectory() }))],
       }),
     ),
     Effect.provide(NodeServices.layer),
@@ -102,7 +99,7 @@ const processEffect = Effect.fnUntraced(function* (options: Options) {
             fetch: !truthy(process.env.OPENCODE_DISABLE_MODELS_FETCH),
           },
           config: {
-            directory: process.env.OPENCODE_CONFIG_DIR,
+            directory: configDirectory(),
             project: !truthy(
               process.env.OPENCODE_CONFIG_PROJECT_DISABLE ?? process.env.OPENCODE_DISABLE_PROJECT_CONFIG,
             ),
