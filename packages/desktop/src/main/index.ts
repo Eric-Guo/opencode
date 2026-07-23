248380b14a55dfc80422001d22cd6d9c0c7d165f
Eric-Guo <eric.guocz@gmail.com>
2026-07-23 09:32:19 +0800

    feat(desktop): add navigation history to desktop menus

--- packages/desktop/src/main/index.ts
+++ packages/desktop/src/main/index.ts
@@ -36,13 +36,16 @@ import {
 import { setupAutoUpdater, showUpdaterDialog } from "./updater"
 import { safeWebContentsURL } from "./window-state"
 import {
+  getDesktopTabHistory,
   getLastFocusedWindow,
+  goToDesktopTabHistory,
   registerRendererProtocol,
   setRelaunchHandler,
   setAppQuitting,
   setBackgroundColor,
   setDockIcon,
   restoreMainWindows,
+  subscribeDesktopTabHistory,
 } from "./windows"
 import { createWslServersController } from "./wsl/servers"
 import { registerWslIpcHandlers } from "./wsl/ipc"
@@ -417,6 +420,9 @@ const main = Effect.gen(function* () {
       relaunch: () => {
         relaunch()
       },
+      getHistory: () => getDesktopTabHistory(getLastFocusedWindow()),
+      goToHistory: (index) => goToDesktopTabHistory(getLastFocusedWindow(), index),
+      onHistoryChange: subscribeDesktopTabHistory,
     })
   }
 })
