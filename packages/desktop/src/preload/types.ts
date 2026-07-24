84de987b3940f79fcc5dca3577268235e342c242
Eric-Guo <eric.guocz@gmail.com>
2026-07-24 14:14:25 +0800

    Routing getCybrosCurrentUser request through Electron’s main process

--- packages/desktop/src/preload/types.ts
+++ packages/desktop/src/preload/types.ts
@@ -26,6 +26,11 @@ export type ServerReadyData = {
   suggestedQuestions?: string[]
 }
 
+export type CybrosCurrentUser = {
+  chinese_name: string
+  clerk_code: string
+}
+
 export type WslServersAPI = WslServersPlatform
 export type UpdaterAPI = {
   subscribe: (cb: (state: UpdaterState) => void) => Promise<() => void>
@@ -49,6 +54,7 @@ export type FatalRendererError = {
 export type ElectronAPI = {
   killSidecar: () => Promise<void>
   awaitInitialization: () => Promise<ServerReadyData>
+  getCybrosCurrentUser: () => Promise<CybrosCurrentUser>
   wslServers: WslServersAPI
   updater: UpdaterAPI
   consumeInitialDeepLinks: () => Promise<string[]>
