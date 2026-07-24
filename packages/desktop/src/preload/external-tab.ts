504ac1c9e3ad3206ca3b73301e1d6805d34e8c9f
Eric-Guo <eric.guocz@gmail.com>
2026-07-24 14:14:25 +0800

    Routing getCybrosCurrentUser request through Electron’s main process

--- packages/desktop/src/preload/external-tab.ts
+++ packages/desktop/src/preload/external-tab.ts
@@ -1,8 +1,9 @@
 import { contextBridge, ipcRenderer } from "electron"
 import type { ElectronAPI } from "./types"
 
-const api: Pick<ElectronAPI, "awaitInitialization"> = {
+const api: Pick<ElectronAPI, "awaitInitialization" | "getCybrosCurrentUser"> = {
   awaitInitialization: () => ipcRenderer.invoke("await-initialization"),
+  getCybrosCurrentUser: () => ipcRenderer.invoke("get-cybros-current-user"),
 }
 
 contextBridge.exposeInMainWorld("api", api)
