48ccf162dc57d8fcb654d9242c8f7fcc9835d292
Eric-Guo <eric.guocz@gmail.com>
2026-07-28 22:55:13 +0800

    Build desktop CLI binaries from local targets

--- packages/desktop/scripts/predev.ts
+++ packages/desktop/scripts/predev.ts
@@ -1,10 +1,12 @@
 import { $ } from "bun"
 import { cp, mkdir, rm } from "node:fs/promises"
+import { buildCliToResources } from "./utils"
 
 await $`bun run install-electron`
 
 await $`bun ./scripts/copy-icons.ts ${process.env.OPENCODE_CHANNEL ?? "dev"}`
 
+await buildCliToResources()
 await $`cd ../opencode && bun script/bundle-node.ts`
 await $`cd ../7777 && bun run build`
 await rm("out/renderer/7777", { recursive: true, force: true })
