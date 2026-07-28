836e3b4f28d1eb23291c96a7e508f2bb20b49582
Eric-Guo <eric.guocz@gmail.com>
2026-07-28 22:55:13 +0800

    Build desktop CLI binaries from local targets

--- packages/desktop/scripts/prebuild.ts
+++ packages/desktop/scripts/prebuild.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env bun
 import { $ } from "bun"
 
-import { resolveChannel } from "./utils"
+import { buildCliToResources, resolveChannel } from "./utils"
 
 const channel = resolveChannel()
 await $`bun ./scripts/copy-icons.ts ${channel}`
 await $`bun ./scripts/copy-metainfo.ts ${channel}`
 
+if (channel === "dev") await buildCliToResources()
 await $`cd ../opencode && bun script/bundle-node.ts`
 await $`cd ../7777 && bun run build`
