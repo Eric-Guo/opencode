db121639675e5a0cc3af90fd6744692455899252
Eric-Guo <eric.guocz@gmail.com>
2026-07-29 22:39:46 +0800

    fix(protocol): type global config responses as JSON

--- packages/client/src/promise/generated/types.ts
+++ packages/client/src/promise/generated/types.ts
@@ -385,6 +385,8 @@ export type WebSearchProvider = { id: string; name: string }
 
 export type WebSearchResult = { url: string; title?: string; content?: string; time: { published?: number } }
 
+export type GlobalConfig = { [x: string]: JsonValue }
+
 export type SessionMessageModelSelected = {
   id: string
   metadata?: { [x: string]: JsonValue }
@@ -5806,4 +5808,4 @@ export type ConfigGetInput = {
 
 export type ConfigGetOutput = Array<ConfigEntry>
 
-export type ConfigGlobalOutput = { [x: string]: any }
+export type ConfigGlobalOutput = GlobalConfig
