8e1e6c760665a7af1e913c3a79898063cc95744e
Eric-Guo <eric.guocz@gmail.com>
2026-07-30 15:31:50 +0800

    feat(cli): add CORS configuration to service settings

--- packages/cli/src/server-process.ts
+++ packages/cli/src/server-process.ts
@@ -75,6 +75,7 @@ const processEffect = Effect.fnUntraced(function* (options: Options) {
           hostname,
           port,
           password,
+          cors: config.cors,
           simulation: truthy(process.env.OPENCODE_SIMULATE),
           database: {
             path:
