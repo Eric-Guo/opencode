7e4e24a56e2d4dd925d78318aa92c072d505ffd7
Eric-Guo <eric.guocz@gmail.com>
2026-07-30 12:26:32 +0800

    feat(desktop): embed sidecar server in Electron app

--- packages/desktop/scripts/prebuild.ts
+++ packages/desktop/scripts/prebuild.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env bun
 import { $ } from "bun"
 
-import { buildCliToResources, resolveChannel } from "./utils"
+import { buildEmbeddedSidecar, resolveChannel } from "./utils"
 
 const channel = resolveChannel()
 await $`bun ./scripts/copy-icons.ts ${channel}`
 await $`bun ./scripts/copy-metainfo.ts ${channel}`
 
-if (channel === "dev") await buildCliToResources()
+await buildEmbeddedSidecar()
 await $`cd ../7777 && bun run build`
