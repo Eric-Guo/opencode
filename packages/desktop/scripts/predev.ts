a45a8c7bfaf46d9d6b73944459b225518982dc1c
Eric-Guo <eric.guocz@gmail.com>
2026-07-30 12:26:32 +0800

    feat(desktop): embed sidecar server in Electron app

--- packages/desktop/scripts/predev.ts
+++ packages/desktop/scripts/predev.ts
@@ -1,12 +1,12 @@
 import { $ } from "bun"
 import { cp, mkdir, rm } from "node:fs/promises"
-import { buildCliToResources } from "./utils"
+import { buildEmbeddedSidecar } from "./utils"
 
 await $`bun run install-electron`
 
 await $`bun ./scripts/copy-icons.ts ${process.env.OPENCODE_CHANNEL ?? "dev"}`
 
-await buildCliToResources()
+await buildEmbeddedSidecar()
 await $`cd ../7777 && bun run build`
 await rm("out/renderer/7777", { recursive: true, force: true })
 await mkdir("out/renderer", { recursive: true })
