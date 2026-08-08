c055ff41f84a7508294be03bf5fc65d56884e6c6
Eric-Guo <eric.guocz@gmail.com>
2026-08-08 16:08:53 +0800

    fix(core): repair V2 table foreign keys

--- packages/core/src/database/migration.gen.ts
+++ packages/core/src/database/migration.gen.ts
@@ -40,8 +40,9 @@ import m37 from "./migration/20260622202450_simplify_session_input.js"
 import m38 from "./migration/20260804233008_loose_psylocke.js"
 import m39 from "./migration/20260805200742_import_legacy_credentials.js"
 import m40 from "./migration/20260808023530_workspace_domain.js"
-import m41 from "./migration/20260811161259_execution_claim_attempts.js"
-import m42 from "./migration/20260812181746_session_inbox.js"
+import m41 from "./migration/20260808090000_repair_v2_foreign_keys.js"
+import m42 from "./migration/20260811161259_execution_claim_attempts.js"
+import m43 from "./migration/20260812181746_session_inbox.js"
 
 export const migrations = [
   m00,
@@ -87,4 +88,5 @@ export const migrations = [
   m40,
   m41,
   m42,
+  m43,
 ] satisfies DatabaseMigration.Migration[]
