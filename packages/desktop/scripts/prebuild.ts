d39367ee5fae306a3151fa1ff2b309b5fb7a8677
Eric-Guo <eric.guocz@gmail.com>
2026-08-08 14:21:31 +0800

    refactor(desktop): launch background CLI service

--- packages/desktop/scripts/prebuild.ts
+++ packages/desktop/scripts/prebuild.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env bun
 import { $ } from "bun"
 
-import { buildEmbeddedSidecar, resolveChannel } from "./utils"
+import { buildCliToResources, resolveChannel } from "./utils"
 
 const channel = resolveChannel()
 await $`bun ./scripts/copy-icons.ts ${channel}`
 await $`bun ./scripts/copy-metainfo.ts ${channel}`
 
-await buildEmbeddedSidecar()
+if (channel === "dev") await buildCliToResources()
 await $`cd ../7777 && bun run build`
