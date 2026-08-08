f8cf62bdb37af419d3140fc01e0db0395884445b
Eric-Guo <eric.guocz@gmail.com>
2026-08-08 16:08:53 +0800

    fix(core): repair V2 table foreign keys

--- packages/core/src/database/migration.gen.ts
+++ packages/core/src/database/migration.gen.ts
@@ -40,13 +40,14 @@ import m37 from "./migration/20260622202450_simplify_session_input.js"
 import m38 from "./migration/20260804233008_loose_psylocke.js"
 import m39 from "./migration/20260805200742_import_legacy_credentials.js"
 import m40 from "./migration/20260808023530_workspace_domain.js"
-import m41 from "./migration/20260811161259_execution_claim_attempts.js"
-import m42 from "./migration/20260812181746_session_inbox.js"
-import m43 from "./migration/20260812213948_worktree.js"
-import m44 from "./migration/20260819222447_session_viewed_state.js"
-import m45 from "./migration/20260823191254_nullable_workspace_binding.js"
-import m46 from "./migration/20260910120000_clear_v1_session_permission.js"
-import m47 from "./migration/20260923013825_project_time_active.js"
+import m41 from "./migration/20260808090000_repair_v2_foreign_keys.js"
+import m42 from "./migration/20260811161259_execution_claim_attempts.js"
+import m43 from "./migration/20260812181746_session_inbox.js"
+import m44 from "./migration/20260812213948_worktree.js"
+import m45 from "./migration/20260819222447_session_viewed_state.js"
+import m46 from "./migration/20260823191254_nullable_workspace_binding.js"
+import m47 from "./migration/20260910120000_clear_v1_session_permission.js"
+import m48 from "./migration/20260923013825_project_time_active.js"
 
 export const migrations = [
   m00,
@@ -97,4 +98,5 @@ export const migrations = [
   m45,
   m46,
   m47,
+  m48,
 ] satisfies DatabaseMigration.Migration[]
