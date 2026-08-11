ecdf088d7bab47b101aa4160dfd8349d584cf364
Eric-Guo <eric.guocz@gmail.com>
2026-08-11 12:20:07 +0800

    fix(cli): support Node SEA runtime and clean shutdown
    Bump to 26.8.1

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -121,6 +121,5 @@ Effect.gen(function* () {
   ),
   Effect.provide(NodeServices.layer),
   Effect.scoped,
-  Effect.tap(() => Effect.sync(() => process.exit(process.exitCode ?? 0))),
   NodeRuntime.runMain,
 )
