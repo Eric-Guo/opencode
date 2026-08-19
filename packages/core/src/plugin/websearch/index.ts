c4bec17b949c6135bb9ee31f0695b7f9dd74a350
Eric-Guo <eric.guocz@gmail.com>
2026-08-19 14:50:15 +0800

    Add SearchKimi web search provider

--- packages/core/src/plugin/websearch/index.ts
+++ packages/core/src/plugin/websearch/index.ts
@@ -1,11 +1,13 @@
 import { WebSearchExa } from "./exa.js"
 import { WebSearchFirecrawl } from "./firecrawl.js"
 import { WebSearchParallel } from "./parallel.js"
+import { SearchKimi } from "./searchkimi.js"
 import { WebSearchTavily } from "./tavily.js"
 
 export const WebSearchPlugins = [
   WebSearchExa.Plugin,
   WebSearchFirecrawl.Plugin,
   WebSearchParallel.Plugin,
+  SearchKimi.Plugin,
   WebSearchTavily.Plugin,
 ] as const
