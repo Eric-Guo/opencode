9847211b485def8a83e69e4861b6234da8afd64a
Eric-Guo <eric.guocz@gmail.com>
2026-08-20 13:06:11 +0800

    feat: hide SSO-configured agents from local agent selection

--- packages/server/test/process.test.ts
+++ packages/server/test/process.test.ts
@@ -155,6 +155,7 @@ it.live("allows browser preflight requests without credentials", () =>
     expect(configBody).toMatchObject({
       username: "Test User",
       clerk_code: "123456",
+      hide_agents: [],
       agents: {
         build: {
           permissions: [{ action: "websearch", resource: "*", effect: "allow" }],
