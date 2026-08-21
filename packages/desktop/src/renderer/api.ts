8f08927ee6a47718c98e12cc7e909ffd2a3e7a24
Eric-Guo <eric.guocz@gmail.com>
2026-08-21 12:08:26 +0800

    Add Cybros current-user IPC endpoint

--- packages/desktop/src/renderer/api.ts
+++ packages/desktop/src/renderer/api.ts
@@ -30,6 +30,7 @@ export const api: ElectronAPI = {
     send: (request) => send("BrowserPane", { request }),
     onEvent: (callback) => listen("BrowserPaneEvent", (value) => callback(value)),
   },
+  getCybrosCurrentUser: () => invoke("AppGetCybrosCurrentUser").then(mutable),
   signInToThapeSso: (credentials) => invoke("AppSignInToThapeSso", { credentials }),
   wslServers: {
     getState: () => invoke("WslGetState").then(mutable),
