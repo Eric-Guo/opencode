0b3cb913ef1d7ed99a81892ffab695094758ce13
Eric-Guo <eric.guocz@gmail.com>
2026-08-24 11:25:42 +0800

    Revert "Embed the desktop sidecar and remove CLI installation flow"
    This reverts commit 759f4e4849a2b479998967cf179b92c935ed9645.

--- packages/desktop/src/main/index.ts
+++ packages/desktop/src/main/index.ts
@@ -7,6 +7,7 @@ import { Ipc } from "./ipc"
 import { DesktopInitialization } from "./lifecycle/desktop-initialization"
 import { ApplicationLifecycle } from "./lifecycle"
 import { BackgroundService } from "./service/background-service"
+import { DesktopCli } from "./service/desktop-cli"
 import { UpdaterLive } from "./updater/live"
 
 const runIpc = Effect.fn("Desktop.runIpc")(function* () {
@@ -23,6 +24,7 @@ const runIpc = Effect.fn("Desktop.runIpc")(function* () {
 runIpc().pipe(
   Effect.provide(Ipc.layer),
   Effect.provide(BackgroundService.layer),
+  Effect.provide(DesktopCli.layer),
   Effect.provide(UpdaterLive.layer),
   Effect.provide(DesktopInitialization.layer),
   Effect.provide(ApplicationLifecycle.layer),
