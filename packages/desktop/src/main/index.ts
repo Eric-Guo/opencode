3e6924dd443e3d1357a276807391158e9d1e89d8
Eric-Guo <eric.guocz@gmail.com>
2026-08-24 11:25:07 +0800

    Embed the desktop sidecar and remove CLI installation flow

--- packages/desktop/src/main/index.ts
+++ packages/desktop/src/main/index.ts
@@ -7,7 +7,6 @@ import { Ipc } from "./ipc"
 import { DesktopInitialization } from "./lifecycle/desktop-initialization"
 import { ApplicationLifecycle } from "./lifecycle"
 import { BackgroundService } from "./service/background-service"
-import { DesktopCli } from "./service/desktop-cli"
 import { UpdaterLive } from "./updater/live"
 
 const runIpc = Effect.fn("Desktop.runIpc")(function* () {
@@ -24,7 +23,6 @@ const runIpc = Effect.fn("Desktop.runIpc")(function* () {
 runIpc().pipe(
   Effect.provide(Ipc.layer),
   Effect.provide(BackgroundService.layer),
-  Effect.provide(DesktopCli.layer),
   Effect.provide(UpdaterLive.layer),
   Effect.provide(DesktopInitialization.layer),
   Effect.provide(ApplicationLifecycle.layer),
