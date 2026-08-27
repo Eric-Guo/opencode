1d31c09fed5bbc7a66e3993c57ec17fc8f958810
Eric-Guo <eric.guocz@gmail.com>
2026-08-28 07:26:29 +0800

    Join active session executions before filesystem work

--- packages/core/test/session-create.test.ts
+++ packages/core/test/session-create.test.ts
@@ -1367,7 +1367,7 @@ describe("SessionTransfer", () => {
                   state: { thoughtSignature: "secret" },
                 },
               ],
-              time: { created: DateTime.makeUnsafe(101) },
+              time: { created: DateTime.makeUnsafe(101), completed: DateTime.makeUnsafe(102) },
             },
             {
               id: errorMessageID,
