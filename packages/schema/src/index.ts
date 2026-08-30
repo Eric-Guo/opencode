e0578a623a25a6e1d99caac33d2dbca14b469272
Eric-Guo <eric.guocz@gmail.com>
2026-08-30 12:04:33 +0800

    Implemented the process-wide audio recorder end to end

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,4 +1,5 @@
 export { Agent } from "./agent.js"
+export { Audio } from "./audio.js"
 export { Command } from "./command.js"
 export { Config } from "./config.js"
 export { Connection } from "./connection.js"
